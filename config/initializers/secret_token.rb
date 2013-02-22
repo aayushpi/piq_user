685edc2a43fedac9c05212b9366cbfe36d60f85e
Aayush Iyer <aayushiyer@Aayushs-MacBook-Pro.local>
2013-02-22 12:55:45 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiqUser::Application.config.secret_token = '549d909a93c5f88d0793cb30f7f70ecf720b4a53b28110deb6ae1f4150b8756d067080d8fdd856fab7a24318cc55e9f072db32b732199b9501f8506c06a6a46a'
